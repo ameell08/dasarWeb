2566249cd67b6d0283ba0cb64f357523390846e2
Amel08SIB <amelia.safitri.wahyu@gmail.com>
2024-03-05 12:50:51 +0700

    pertemuan4-praktikum 3

---
+++ pertemuan4/operator.php
@@ -0,0 +1,66 @@
+<?php
+$a =10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$pangkat = $a ** $b;
+
+echo "Hasil Tambah : {$hasilTambah} <br>";
+echo "Hasil Kurang : {$hasilKurang} <br>";
+echo "Hasil Kali : {$hasilKali} <br>";
+echo "Hasil Bagi : {$hasilBagi} <br>";
+echo "Hasil Pangkat : {$pangkat} <br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b ;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b ;
+$hasilLebihBesarSama = $a >= $b ;
+
+echo "Hasil Sama : {$hasilSama} <br>";
+echo "Hasil tidak sama : {$hasilTidakSama} <br>";
+echo "Hasil lebih kecil : {$hasilLebihKecil} <br>";
+echo "Hasil lebih besar : {$hasilLebihBesar} <br>";
+echo "Hasil lebih kecil sama dengan : {$hasilLebihKecilSama} <br>";
+echo "Hasil lebih besar sama dengan : {$hasilLebihBesarSama} <br>";
+
+$hasilAnd = $a && $b;
+$hasilOr = $a || $b;
+$hasilNotA = !$a;
+$hasilNotB = !$b;
+
+echo "Hasil And : {$hasilAnd} <br>";
+echo "Hasil Or : {$hasilOr} <br>";
+echo "Hasil Not A : {$hasilNotA} <br>";
+echo "Hasil Not B : {$hasilNotB} <br>";
+
+$hasilTambahSama = $a += $b ;
+$hasilKurangSama = $a -= $b ;
+$hasilKaliSama = $a *= $b ;
+$hasilBagiSama = $a /= $b ;
+$hasilSisaBagiSama = $a %= $b ;
+echo "<br>";
+echo "Hasil tambah sama : {$hasilTambahSama} <br>";
+echo "Hasil kurang sama : {$hasilKurangSama} <br>";
+echo "Hasil kali sama : {$hasilKaliSama} <br>";
+echo "Hasil bagi sama : {$hasilBagiSama} <br>";
+echo "Hasil sisa bagi sama : {$hasilSisaBagiSama} <br>";
+
+$hasilIdentik = $a === $b;
+$hasilTidakIdentik = $a !== $b;
+echo "<br>";
+echo "Hasil identik : {$hasilIdentik} <br>";
+echo "Hasil tidak identik  : {$hasilTidakIdentik} <br>";
+
+$c = 45;
+$d = 28;
+$kursiTdkTerpakai = $c - $d ;
+$persenKursi = $kursiTdkTerpakai * 100 / $c;
+echo "<br>";
+echo "jumlah Kursi kosonng : {$kursiTdkTerpakai } <br>";
+echo "Hasil persen : {$persenKursi} <br>";
+?>
