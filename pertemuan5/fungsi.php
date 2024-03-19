3caf5b54eeb5765ad4fceb16f08525cfa1a971a9
Amel08SIB <amelia.safitri.wahyu@gmail.com>
2024-03-19 12:19:09 +0700

    pertemuan5_fungsi

--- pertemuan5/fungsi.php
+++ pertemuan5/fungsi.php
@@ -3,6 +3,17 @@
 //membuat fungsi
 function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
-    return $umur; }
-    echo "Umur saya adalah " . hitungUmur(2003, 2023) ."tahun"
+    return $umur; 
+}
+function perkenalan ($nama,  $salam="Assalamualaikum")    {
+    echo $salam. ",";
+    echo "perkenalkan, nama saya ". $nama. "<br/>";
+
+    //memanggil fungsi lain
+    echo "Saya berusia ". hitungUmur(2003, 2023). " tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+// memanggoil fungsi perkenalan
+perkenalan("Amel");
 ?>
