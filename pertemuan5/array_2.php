1b1d7b4ed27e2ba513577d66fd5959c75cbddecb
Amel08SIB <amelia.safitri.wahyu@gmail.com>
2024-03-19 11:38:36 +0700

    pertemuan5_praktikum 2

--- pertemuan5/array_2.php
+++ pertemuan5/array_2.php
@@ -4,16 +4,42 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, intitial-scale=1">
         <title></title>
+        <style>
+            table {
+                border-collapse: collapse;
+                width: 50%;
+                margin: auto;
+                background-color: #7FFFD4;
+                
+            }
+
+            th, td {
+                border: 1px solid #00008B;
+                text-align: left;
+                padding: 8px;
+            }
+
+        </style>
     </head>
     <body>
         <?php $Dosen = [
             'nama' => 'Elok Nur Hamdana',
             'domisili' => 'Malang',
             'jenis_kelamin' => 'Perempuan' ];
-        
-            echo "Nama : {$Dosen ['nama'] } <br>";
-            echo "Domisili : {$Dosen ['domisili'] } <br>";
-            echo "Jenis kelamin : {$Dosen ['jenis_kelamin'] } <br>";
+    
             ?>
+            <table>
+            <tr>
+                <td>Nama</td>
+                <td><?php echo $Dosen['nama']; ?></td>
+            </tr>
+            <tr>
+                <td>Domisili</td>
+                <td><?php echo $Dosen['domisili']; ?></td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+            </tr>
     </body>
 </html>
