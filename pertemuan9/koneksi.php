d88c273de5660f83d195aab8e9d870ace2541b70
Amel08SIB <amelia.safitri.wahyu@gmail.com>
2024-04-09 04:19:40 +0700

    pertemuan9-praktikum 2

---
+++ pertemuan9/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+// Koneksi ke database
+$connect = mysqli_connect("localhost", "root", "" , "prakwebdb");
+
+// periksa koneksi
+if (mysqli_connect_errno()){
+    die("koneksi database gagal : " . mysqli_connect_error());
+}
+?>
