1f0c627072f9733d5886f4c07498046131f910da
Amel08SIB <amelia.safitri.wahyu@gmail.com>
2024-04-20 22:47:10 +0700

    pertemuan10-praktikum 2

--- pertemuan10/crud/proses.php
+++ pertemuan10/crud/proses.php
@@ -8,16 +8,14 @@ $alamat = $_POST['alamat'];
 $no_telp = $_POST['no_telp'];
 
 if($aksi == 'tambah'){
-    $query = "INSERT INTO anggota (nama, jenis_kelamin, alamat, no_telp) VALUES 
-    ('$nama', '$jenis_kelamin', '$alamat', '$no_telp')";
+    $query ="INSERT INTO anggota (nama, jenis_kelamin, alamat, no_telp) VALUES ('$nama', '$jenis_kelamin', '$alamat', '$no_telp')";
 
     if (mysqli_query($koneksi, $query)){
         header("Location: index.php");
         exit();
     } else {
-        echo "Gagal menambahkan data: " . mysqli_error($koneksi);
+        echo "Gagal menambahkan data : " . mysqli_error($koneksi);
     }
 }
-
 mysqli_close($koneksi);
 ?>
